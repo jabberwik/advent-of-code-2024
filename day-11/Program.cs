fe4a51b6b97053ddd95de487135c3f98e3c789c5
Daniel Laughland <daniel@laughland.com>
2024-12-13 01:57:50 -0600

    Improved Day 11 solution

--- day-11/Program.cs
+++ day-11/Program.cs
@@ -2,39 +2,35 @@
  * Advent of Code 2024, Day 11
  */
 
-var input = File.ReadAllText("input.txt").Split(' ').Select(long.Parse);
+var input = File.ReadAllText("input.txt").Split(' ').Select(long.Parse).ToArray();
 
-var stones = new LinkedList<long>();
-foreach (var stone in input) stones.AddLast(stone);
+var part1Solution = input.AsParallel().Sum(x => Blink(x, 25));
+Console.WriteLine($"Part 1: {part1Solution}");
 
-foreach (var blink in Enumerable.Range(1, 75))
+var part2Solution = input.AsParallel().Sum(x => Blink(x, 75));
+Console.WriteLine($"Part 2: {part2Solution}");
+
+return;
+
+long Blink(long stone, int blinkCount, int blinks = 0)
 {
-    var stone = stones.First;
-    while (stone != null)
+    while (blinks < blinkCount)
     {
-        // If the stone has a 0, replace it with a 1
-        if (stone.Value == 0)
+        if (stone == 0)
         {
-            stone.ValueRef = 1;
-            stone = stone.Next;
+            stone = 1;
+            blinks += 1;
+            continue;
         }
-        // If the stone has an even number of digits, split it into two halves of that number
-        else if (stone.Value.ToString().Length % 2 == 0)
-        {
-            var digits = stone.Value.ToString();
-            var left = long.Parse(digits[..(digits.Length / 2)]);
-            var right = long.Parse(digits[(digits.Length / 2)..]);
-            var leftStone = stones.AddAfter(stone, left);
-            var rightStone = stones.AddAfter(leftStone, right);
-            stones.Remove(stone);
-            stone = rightStone.Next;
-        }
-        else
-        {
-            stone.ValueRef = stone.Value * 2024;
-            stone = stone.Next;
-        }
+
+        var digits = stone.ToString();
+        if (digits.Length % 2 == 0)
+            return Blink(long.Parse(digits[..(digits.Length / 2)]), blinkCount, blinks + 1) +
+                   Blink(long.Parse(digits[(digits.Length / 2)..]), blinkCount, blinks + 1);
+
+        stone *= 2024;
+        blinks += 1;
     }
-    
-    Console.WriteLine($"After {blink}: {stones.Count}");
+
+    return 1;
 }
